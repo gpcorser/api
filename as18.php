d33176d15a003979b58f238a29672d09d3dd650f
George Corser <gpcorser@svsu.edu>
2021-04-07 07:58:41 -0400

    initial commit

---
+++ as18.php
@@ -0,0 +1,35 @@
+<?php 
+echo "<a target='_blank' href='https://github.com/gpcorser/api.git'>GitHub repo</a> <br>";
+
+main(); 
+
+function main () {
+	
+	$apiCall = 'https://api.covid19api.com/summary';
+	$json_string = curl_get_contents($apiCall);
+	$obj = json_decode($json_string);
+
+    $arr1 = Array();
+    $arr2 = Array();
+    foreach($obj->Countries as $i) {
+        array_push($arr1, $i->Country );
+        array_push($arr2, $i->TotalDeaths);
+    }
+    array_multisort($arr2, SORT_DESC, $arr1);
+    print_r($arr1); 
+
+}
+
+#FUNCTIONS
+#-----------------------------------------------------------------------------
+// read data from a URL into a string
+function curl_get_contents($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
